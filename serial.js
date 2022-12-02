4c11c5a80f65d3a4dfb4fbcfea226a238d456925
David <dav.gruber@htlstp.at>
2022-12-02 19:14:05 +0100

    feat: replace rx by parser

--- serial.js
+++ serial.js
@@ -1,31 +1,32 @@
 const {SerialPort} = require("serialport");
-const { ReadlineParser } = require('@serialport/parser-readline')
+const {ReadlineParser} = require('@serialport/parser-readline')
 const {SerialPortMock} = require("serialport");
 
 //hc12 module config
 const hc12 = {
-    interface: '/dev/serial0',
-    baudRate: 9600,
+    interface: '/dev/serial0', baudRate: 9600,
 };
 
 port = new SerialPort({
-    path: hc12.interface,
-    baudRate: hc12.baudRate,
-    autoOpen: false
+    path: hc12.interface, baudRate: hc12.baudRate, autoOpen: false
 });
 
-globals = { SerialPort, SerialPortMock, port};
+globals = {SerialPort, SerialPortMock, port};
 
 const parser = port.pipe(new ReadlineParser({
     delimiter: '\n'
 }));
 
-port.open(err => console.log(err));
+port.open(err => {
+    if (err != null) console.log(err);
+});
+
+parser.on('data', data => {
+    console.log(data);
+});
 
 tx_task = setInterval(() => {
-    port.write('Hello World', err => console.log(err));
-}, 2000);
-
-rx_task = setInterval(() => {
-    console.log("rx: ", port.read())
+    port.write('Hello World\n', err => {
+        if (err != null) console.log("tx: ", err);
+    });
 }, 2000);
