19c4bb8afc3b80629cd9f76b6e17aaa34322d3ce
David <dav.gruber@htlstp.at>
2022-12-02 18:39:45 +0100

    fix: parser

--- serial.js
+++ serial.js
@@ -1,20 +1,29 @@
-const SerialPort = require("serialport");
-const Readline = require("readline");
+const {SerialPort} = require("serialport");
+const { ReadlineParser } = require('@serialport/parser-readline')
+const {SerialPortMock} = require("serialport");
 
+//hc12 module config
 const hc12 = {
     interface: '/dev/serial0',
     baudRate: 9600,
-}
+};
 
-let serialPort = SerialPort(hc12.interface, {
-    baudRate: hc12.baudRate
+port = new SerialPort({
+    path: hc12.interface,
+    baudRate: hc12.baudRate,
+    autoOpen: false
 });
 
-const parser = serialPort.pipe(new Readline({
+globals = { SerialPort, SerialPortMock, port};
+
+const parser = port.pipe(new ReadlineParser({
     delimiter: '\n'
-}))
+}));
+
+port.open();
 
 parser.on('data', (data) => {
-    console.log(data);
-})
+    console.log(data.toString());
+});
 
+port.write('Hello World');
