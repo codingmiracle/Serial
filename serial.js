583756bc25dd9e8cb97f33035e1fd30d2bdddd4f
David <dav.gruber@htlstp.at>
2022-12-03 18:40:16 +0100

    impl: packet-parser

--- serial.js
+++ serial.js
@@ -1,11 +1,19 @@
 const {SerialPort, PacketLengthParser} = require("serialport");
 const {SerialPortMock} = require("serialport");
+const {crypto} = require("crypto");
 
 //hc12 module config
 const hc12 = {
     interface: '/dev/serial0',
     baudRate: 9600
 };
+
+const aes_context = {
+    algorithm: 'aes-256-cbc',
+    key: crypto.randomBytes(256).toString(),
+    iv: "000000000000000\0"
+}
+console.log(aes_context);
 
 port = new SerialPort({
     path: hc12.interface, baudRate: hc12.baudRate, autoOpen: false
@@ -19,19 +27,17 @@ const parser = port.pipe(new PacketLengthParser({
     maxLen: 1024
 }));
 
+const decipher = crypto.createDecipheriv('aes-256-cbc', aes_context.key, aes_context.iv);
 
 globals = {SerialPort, SerialPortMock, port};
 
 init = () => {
     port.on('error', console.log);
 
-    port.open(err => {
-        if (err != null) console.log(err);
-        else port.write(aes_context.key);
-    });
+    port.open();
 
     parser.on('data', data => {
-        console.log(data);
+        console.log(data.toString());
     });
 
     tx_task = setInterval(() => {
